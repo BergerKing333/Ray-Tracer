36c0e356065a791362ad61501305ab6af5dfe21c
BergerKing <BergerKing@users.noreply.github.com>
2024-08-20 01:20:41 -0500

    Working metals and dielectrics

--- RayTracing/camera.h
+++ RayTracing/camera.h
@@ -1,75 +1,149 @@
 #pragma once
 
 using color = vec3;
+#include "material.h"
 
 class camera {
 public:
 	double aspect_ratio;
 	int image_width;
-	int    image_height;   // Rendered image height
+	int image_height;   // Rendered image height
+	int samples_per_pixel = 10;
+	int maxRayDepth = 10;
 
-	camera(double aspect_ratio, int image_width) : aspect_ratio(aspect_ratio), image_width(image_width) {
-        initialize();
-    }
+	double vfov = 90.0;
+	point3 lookfrom = point3(0, 0, 0);
+	point3 lookat = point3(0, 0, -1);
+	vec3 vup = vec3(0, 1, 0);
+
+	double defocus_angle = 0;
+	double focus_dist = 10;
+
+	camera() {}
 
     void render(cv::Mat& image, const hittable& world) {
+		initialize();
 		for (int j = 0; j < image_height; j++) {
+			if (j % 20 == 0) {
+				std::cout << "Scanlines remaining: " << image_height - j << '\n';
+			}
 			for (int i = 0; i < image_width; i++) {
-				vec3 pixel_center = pixel00_loc + i * pixel_delta_u + j * pixel_delta_v;
-				vec3 ray_direction = pixel_center - center;
-				ray r(center, ray_direction);
-
-				color pixel_color = ray_color(r, world);
-				writeImage(image, j, i, pixel_color);
+				color pixel_color = color(0, 0, 0);
+				for (int sample = 0; sample < samples_per_pixel; sample++) {
+					ray r = get_ray(i, j);
+					pixel_color += ray_color(r, maxRayDepth, world);
+				}
+				writeImage(image, j, i, pixel_color * pixel_samples_scale);
 			}
 		}
 	}
 
 
 private:
-    
+	double pixel_samples_scale;
     point3 center;         // Camera center
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
     vec3   pixel_delta_v;  // Offset to pixel below
 
+	vec3 u, v, w;
+
+	vec3 defocus_disk_u;
+	vec3 defocus_disk_v;
+
     void initialize() {
-        image_height = int(image_width / aspect_ratio);
+        pixel_samples_scale = 1.0 / samples_per_pixel;
+		
+		image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        center = point3(0, 0, 0);
+		center = lookfrom;
 
         // Determine viewport dimensions.
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+		// auto focal_length = (lookfrom - lookat).length();
+		auto theta = degrees_to_radians(vfov);
+		auto h = std::tan(theta / 2);
+		// auto viewport_height = 2 * h * focal_length;
+		auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        auto viewport_u = viewport_width * u;
+		auto viewport_v = viewport_height * -v;
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel.
-        auto viewport_upper_left =
-            center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+		auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+		auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+		defocus_disk_u = defocus_radius * u;
+		defocus_disk_v = defocus_radius * v;
     }
 
 	void writeImage(cv::Mat image, int i, int j, color pixel_color) {
-		int r = pixel_color.x() * 255.999;
-		int g = pixel_color.y() * 255.999;
-		int b = pixel_color.z() * 255.999;
+		static const interval intensity(0, .999);
 
-		image.at<cv::Vec3b>(i, j) = cv::Vec3b(b, g, r);
+		float r = linear_to_gamma(pixel_color.x());
+		float g = linear_to_gamma(pixel_color.y());
+		float b = linear_to_gamma(pixel_color.z());
+
+		int rbyte = int(256 * intensity.clamp(r));
+		int gbyte = int(256 * intensity.clamp(g));
+		int bbyte = int(256 * intensity.clamp(b));
+
+		image.at<cv::Vec3b>(i, j) = cv::Vec3b(bbyte, gbyte, rbyte);
 	}
 
-	color ray_color(const ray& r, const hittable& world) {
+	inline double linear_to_gamma(double linear) {
+		if (linear > 0) {
+			return std::sqrt(linear);
+		}
+		return 0;
+	}
+
+	ray get_ray(int i, int j) {
+		vec3 offset = vec3(random_double() - 0.5, random_double() - 0.5, 0);
+		// If not using anti-aliasing:
+		if (samples_per_pixel == 1) {
+			offset = vec3(0, 0, 0);
+		}
+
+		vec3 pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
+		
+		vec3 ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+		vec3 direction = pixel_sample - ray_origin;
+
+		return ray(ray_origin, direction);
+	}
+
+	point3 defocus_disk_sample() {
+		auto p = random_in_unit_disk();
+		return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+	}
+
+	color ray_color(const ray& r, int depth, const hittable& world) {
+		if (depth <= 0) {
+			return color(0, 0, 0);
+		}
 		hit_record rec;
-		if (world.hit(r, interval(0, infinity), rec)) {
-			return 0.5 * (rec.normal + color(1, 1, 1));
+		if (world.hit(r, interval(0.001, infinity), rec)) {
+			ray scattered;
+			color attenuation;
+
+			if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+				return attenuation * ray_color(scattered, depth - 1, world);
+			}
+			return color(0, 0, 0);
+
+			// return 0.5 * (rec.normal + color(1, 1, 1));
 		}
 
 		vec3 unit_direction = unit_vector(r.direction());
--- RayTracing/utils.h
+++ RayTracing/utils.h
@@ -21,6 +21,11 @@ inline double random_double() {
 	return rand() / (RAND_MAX + 1.0);
 }
 
+inline double random_double(double min, double max) {
+	// Returns a random real in [min,max).
+	return min + (max - min) * random_double();
+}
+
 #include "ray.h"
 #include "vec3.h"
 #include "interval.h"
